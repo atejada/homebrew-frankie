be5da382bcfacc0ef481245bb26532726a689caf
Blag <atejada@gmail.com>
2026-09-08 08:55:36 -0500

    One more time
    Keep trying to upgrade

--- Formula/frankie.rb
+++ Formula/frankie.rb
@@ -1,7 +1,7 @@
 class Frankie < Formula
   desc "Terminal-native language stitched from Ruby, Python, R, and Fortran"
   homepage "https://github.com/atejada/Frankie"
-  url "https://github.com/atejada/Frankie/archive/refs/tags/v1.22.0.tar.gz"
+  url "https://github.com/atejada/Frankie/archive/refs/tags/v1.22.tar.gz"
   sha256 "4706cff4715a70e6e2edbe0545b633e37ce727f5639c8da160ca27277ea2b451"   # see packaging/homebrew/README.md
   license "GPL-3.0-only"
 
